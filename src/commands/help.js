957f30eb882b370b0b7d3dace4e64cb151da7183
Victor Reis David <victorreis5919@gmail.com>
2021-09-19 05:01:29 +0000

    Comando de help. iNovação

---
+++ src/commands/help.js
@@ -0,0 +1,33 @@
+module.exports = {
+ command: {
+  name: "help",
+  description: "Aquela ajudinha com o Searcher, rs."
+ },
+ execute: async function(data) {
+  return {
+   type: Constants.callback_type.MESSAGE,
+   data: {
+    embeds: [
+     {
+       title: "SearcherBot",
+       description: "Bem vindo ao menu de ajuda do Searcher, aqui está alguns tópicos que podem te ajudar:",
+       fields: [
+        {
+         name: "<:noo:886468596363059260> Onde vê os comandos???",
+         value: `https://searcherbot.vercel.app/commands`
+        },
+        {
+         name: "<:pepe_gun:886468234444955738> Achei um bug!1!1!",
+         value: `Calma, calma, não precisa matar o Searcher por isso!! ninguém é perfeito, caso você ache um bug, abra uma issue nesse link: https://github.com/Victoreisdavid/Searcher_bot/issues \n Ou simplesmente avisa no servidor de suporte :thumbsup:`
+        },
+        {
+         name: "<:origin:886471923301744671> E o servidor?",
+         value: `Está aqui: https://discord.gg/fyVcBpfJpF`
+        }
+       ]
+     }
+    ]
+   }
+  }
+ }
+}
