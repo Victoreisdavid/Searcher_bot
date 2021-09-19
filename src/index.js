390c819a0878ad034221ff1f8db11dd9b895d960
Victor Reis David <victorreis5919@gmail.com>
2021-09-19 15:57:50 +0000

    refiz identação

--- src/index.js
+++ src/index.js
@@ -67,32 +67,32 @@ app.post("/api/interaction", async function(req, res) {
    type: 1
   })
  } else if(req.body.type == 3) {
-     const command = commands.get(req.body.message.interaction.name)
-     command.handleInteraction(req.body).then(response => {
+    const command = commands.get(req.body.message.interaction.name)
+    command.handleInteraction(req.body).then(response => {
 		 return res.status(200).json(response)
-		 }).catch(e => {
-		  return res.status(200).json({
-			 type: 4,
-			 data: {
-			  content: `<:shit:887428144469000252> Aconteceu um erro quando você interagiu com a mensagem\n\`\`\`js\n${e}\`\`\``,
-			  flags: 64
-			 }
-			})
-		})
+		}).catch(e => {
+		 return res.status(200).json({
+			type: 4,
+			data: {
+			 content: `<:shit:887428144469000252> Aconteceu um erro quando você interagiu com a mensagem\n\`\`\`js\n${e}\`\`\``,
+			 flags: 64
+			}
+		 })
+	 })
 	} else {
 	   const command = commands.get(req.body.data.name)
 	   command.execute(req.body).then(response => {
 		 return res.status(200).json(response)
-		}).catch(e => {
-		  return res.status(200).json({
-			type: 4,
+		 }).catch(e => {
+      return res.status(200).json({
+			 type: 4,
 			 data: {
 			  content: `<:shit:887428144469000252> Aconteceu um erro\n\`\`\`js\n${e}\`\`\``,
 			  flags: 64
 			 }
 		  })
-		})
-	}
+		 })
+	  }
 })
 
 app.listen(process.env.PORT || 3030)
