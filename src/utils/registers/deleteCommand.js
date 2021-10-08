4a11ce34b64190f172640f176ab9c7c9852eb238
ThePedro <73797168+ThePedroo@users.noreply.github.com>
2021-10-08 20:53:58 -0300

    Update deleteCommands.js
    Atualizando para API v9

--- src/utils/registers/deleteCommand.js
+++ src/utils/registers/deleteCommand.js
@@ -4,27 +4,27 @@ const ID = Config.bot.id
 module.exports = async function (guildID, commandName) {
   console.log("deletando comando:", commandName)
   if (guildID) {
-    let command = await axios.get(`https://discord.com/api/v8/applications/${ID}/guilds/${guildID}/commands`, {
+    let command = await axios.get(`https://discord.com/api/v9/applications/${ID}/guilds/${guildID}/commands`, {
       headers: {
         Authorization: `Bot ${process.env.BOT_TOKEN}`
       }
     })
     command = command.data.find(command => command.name == commandName)
     if (!command) return console.log("Comando não encontrado.")
-    axios.delete(`https://discord.com/api/v8/applications/${ID}/guilds/${guildID}/commands/${command.id}`, {
+    axios.delete(`https://discord.com/api/v9/applications/${ID}/guilds/${guildID}/commands/${command.id}`, {
       headers: {
         Authorization: `Bot ${process.env.BOT_TOKEN}`
       }
     })
   } else {
-    let command = await axios.get(`https://discord.com/api/v8/applications/${ID}/commands`, {
+    let command = await axios.get(`https://discord.com/api/v9/applications/${ID}/commands`, {
       headers: {
         Authorization: `Bot ${process.env.BOT_TOKEN}`
       }
     })
     command = command.data.find(command => command.name == commandName)
     if (!command) return console.log("Comando não encontrado.")
-    axios.delete(`https://discord.com/api/v8/applications/${ID}/commands/${command.id}`, {
+    axios.delete(`https://discord.com/api/v9/applications/${ID}/commands/${command.id}`, {
       headers: {
         Authorization: `Bot ${process.env.BOT_TOKEN}`
       }
