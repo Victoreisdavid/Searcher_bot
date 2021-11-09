1accd8ff97c18cf962890b7f3d93b230f0f35990
Canary2000 <71782887+Canary2000@users.noreply.github.com>
2021-11-09 20:52:27 -0300

    Update API.js
    Adicionando algo para "esconder" as variáveis da propriedade "env".

--- src/apis/API.js
+++ src/apis/API.js
@@ -2,5 +2,10 @@ module.exports = class API {
   constructor(name, env = []) {
     this.name = name;
     this.env = env;
+
+    Object.defineProperty(this, 'env', {
+      enumerable: false,
+      writable: false
+    });
   }
 }
